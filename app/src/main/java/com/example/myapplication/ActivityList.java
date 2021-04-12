fe7947f0ac6ff28e3d03488ecaa0c312faf8d268
mfrfrabbi <mdfazlerabbi.diu@gmail.com>
2021-04-12 12:01:46 +0600

    Add BackButton

--- app/src/main/java/com/example/myapplication/ActivityList.java
+++ app/src/main/java/com/example/myapplication/ActivityList.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -20,21 +21,24 @@ import java.util.List;
 
 
 public class ActivityList extends AppCompatActivity {
-private GridView gridView;
-private String[] countryName;
-private CustomAdapter adapter;
-private SearchView searchView;
-private List<CountryInfo> list;
-private int[] img ={R.drawable.afg,R.drawable.australia,R.drawable.bhutan,
-        R.drawable.bd,R.drawable.china,R.drawable.germany,
-        R.drawable.india,R.drawable.iran,R.drawable.iraq,
-        R.drawable.japan,R.drawable.malaysia,R.drawable.nepal,
-        R.drawable.pak,R.drawable.saudiarabia};
+    private GridView gridView;
+    private String[] countryName;
+    private CustomAdapter adapter;
+    private SearchView searchView;
+    private List<CountryInfo> list;
+    private int[] img = {R.drawable.afg, R.drawable.australia, R.drawable.bhutan,
+            R.drawable.bd, R.drawable.china, R.drawable.germany,
+            R.drawable.india, R.drawable.iran, R.drawable.iraq,
+            R.drawable.japan, R.drawable.malaysia, R.drawable.nepal,
+            R.drawable.pak, R.drawable.saudiarabia};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid);
         this.setTitle("Country Name");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         gridView = findViewById(R.id.gridViewId);
         countryName = getResources().getStringArray(R.array.countryName);
@@ -55,7 +59,7 @@ private int[] img ={R.drawable.afg,R.drawable.australia,R.drawable.bhutan,
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String v = adapter.getItem(position).toString();
-                Toast.makeText(getApplicationContext(),v,Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), v, Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -63,24 +67,31 @@ private int[] img ={R.drawable.afg,R.drawable.australia,R.drawable.bhutan,
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.search_bar_layout,menu);
+        inflater.inflate(R.menu.search_bar_layout, menu);
         MenuItem item = menu.findItem(R.id.searchId);
         SearchView searchView = (SearchView) item.getActionView();
 
-            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                @Override
-                public boolean onQueryTextSubmit(String query) {
-                    return false;
-                }
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
 
-                @Override
-                public boolean onQueryTextChange(String newText) {
-                    adapter.getFilter().filter(newText);
-                    return false;
-                }
-            });
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
 
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+       if(item.getItemId() == android.R.id.home){
+           this.finish();
+       }
+        return super.onOptionsItemSelected(item);
+    }
 }
